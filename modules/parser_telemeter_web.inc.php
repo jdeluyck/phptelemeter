185ae6785e6add9ee5eb59bcbf8a45c871e5bceb
devilkin <devilkin@13be5e6b-9a2a-0410-ba90-e03f93e6454f>
2006-01-21 13:49:59 +0000

    *** empty log message ***
    git-svn-id: https://phptelemeter.svn.sourceforge.net/svnroot/phptelemeter/trunk@38 13be5e6b-9a2a-0410-ba90-e03f93e6454f

--- modules/parser_telemeter_web.inc.php
+++ modules/parser_telemeter_web.inc.php
@@ -63,9 +63,6 @@ class telemeterParser
 							"sso.login.invaliduid" => "Invalid username",
 							"sso.jump.nocookie" => "No cookie detected");
 
-		//$this->months["nl"] = array("januari" => 1, "februari" => 2, "maart" => 3, "april" => 4, "mei" => 5, "juni" => 6, "juli"    => 7, "augustus" => 8, "september" => 9, "oktober" => 10, "november" => 11, "december" => 12);
-		//$this->months["en"] = array("january" => 1, "february" => 2, "march" => 3, "april" => 4, "may" => 5, "june" => 6, "july"    => 7, "august"   => 8, "september" => 9, "october" => 10, "november" => 11, "december" => 12);
-		//$this->months["fr"] = array("janvier" => 1, "février"  => 2, "mars"  => 3, "avril" => 4, "mai" => 5, "juin" => 6, "juillet" => 7, "août"     => 8, "septembre" => 9, "octobre" => 10, "novembre" => 11, "décembre" => 12);
 		$this->months = array("januari" => 1, "februari" => 2, "maart" => 3, "april" => 4, "mei" => 5, "juni" => 6, "juli"    => 7, "augustus" => 8, "september" => 9, "oktober" => 10, "november" => 11, "december" => 12);
 	}
 
@@ -211,8 +208,8 @@ class telemeterParser
 			}
 			
 			$dailyMatches[] = date("d-m-y", $start + (($i - 1) * 86400));
-			$dailyMatches[] = str_replace(".", "", $data3[++$downloadPos] + $data3[++$downloadPos]);
-			$dailyMatches[] = str_replace(".", "", $data3[++$uploadPos] + $data3[++$uploadPos]);
+			$dailyMatches[] = str_replace(".", "", $data3[++$downloadPos]) + str_replace(".","", $data3[++$downloadPos]);
+			$dailyMatches[] = str_replace(".", "", $data3[++$uploadPos]) + str_replace(".","", $data3[++$uploadPos]);
 
 			/* increase pos by one, we don't care for the dates */
 			$downloadPos++;
