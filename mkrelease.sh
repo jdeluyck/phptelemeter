26c819530ae5fc8889809d8ee59ba8b4fdcfbd51
devilkin <devilkin@13be5e6b-9a2a-0410-ba90-e03f93e6454f>
2006-01-28 13:54:33 +0000

    added script to make a release
    git-svn-id: https://phptelemeter.svn.sourceforge.net/svnroot/phptelemeter/trunk@55 13be5e6b-9a2a-0410-ba90-e03f93e6454f

---
+++ mkrelease.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#set -x 
+
+SOURCE="/var/www/phptelemeter"
+TARGET="/tmp"
+
+REL=$(cat phptelemeter.inc.php | grep define\(\"_version | cut -d \" -f 4)
+
+RELNAME="phptelemeter-${REL}"
+
+RELPATH="${TARGET}/${RELNAME}"
+RELTAR="${RELPATH}.tar.gz"
+
+if [ -e ${RELPATH} ]; then
+	echo -n "${RELPATH} exists, delete? [Y/n] "
+	read answer
+
+	case ${answer} in
+		[Yy] | '')
+			rm -rf ${RELPATH}
+			;;
+		*)
+			echo "${RELPATH} exists, cannot continue."
+			exit 1
+			;;
+	esac
+fi
+
+if [ -e ${RELTAR} ]; then
+        echo -n "${RELTAR} exists, delete? [Y/n] "
+        read answer
+
+        case ${answer} in
+                [Yy] | '')
+                        rm -f ${RELTAR}
+                        ;;
+                *)
+                        echo "${RELTAR} exists, cannot continue."
+                        exit 1
+                        ;;
+        esac
+fi
+
+
+mkdir -p ${RELPATH}
+
+cp -r ${SOURCE}/* ${RELPATH}
+
+mv ${RELPATH}/docs/gpl.txt ${RELPATH}
+# clean out
+rm -fr ${RELPATH}/*.session ${RELPATH}/*.webprj ${RELPATH}/CVS ${RELPATH}/modules/CVS ${RELPATH}/modules/libs/CVS ${RELPATH}/docs ${RELPATH}/mkrelease.sh
+
+#tar it up
+pushd /tmp 2>&1 >/dev/null
+tar cfz ${RELTAR} ${RELNAME}
+popd 2>&1 >/dev/null
+echo "Release is available in ${RELPATH}.tar.gz"
