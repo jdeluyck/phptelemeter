2ee9fa43b8aba5064d085ff1ff8250e48fcdb97b
devilkin <devilkin@13be5e6b-9a2a-0410-ba90-e03f93e6454f>
2006-09-08 10:11:29 +0000

    Fixed skynet_web parser when used quota is < 1 gB.
    git-svn-id: https://phptelemeter.svn.sourceforge.net/svnroot/phptelemeter/trunk@106 13be5e6b-9a2a-0410-ba90-e03f93e6454f

--- modules/parser_skynet_web.inc.php
+++ modules/parser_skynet_web.inc.php
@@ -2,7 +2,7 @@
 
 if (! defined("_phptelemeter")) exit();
 
-define("_phptelemeter_parser_skynet_web", "3");
+define("_phptelemeter_parser_skynet_web", "4");
 /*
 
 phpTelemeter - a php script to read out and display ISP's usage-meter stats.
@@ -84,12 +84,18 @@ class telemeterParser_skynet_web extends telemeterParser_web_shared
 		}
 
 		if ($this->debug == true)
+		{
+			echo "DATA:\n";
 			var_dump($data);
+		}
 
 		/* stats */
 		/* total used */
 		$temp = explode(" ", $data[$usedPos]);
-		$volume[] = ($temp[0] * 1024) + $temp[2];
+		if ($temp[1] == "MB")
+			$volume[] = $temp[0] + $temp[2];
+		else
+			$volume[] = ($temp[0] * 1024) + $temp[2];
 
 		/* remaining */
 		$temp = explode(" ", $data[$remainingPos]);
