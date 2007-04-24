493b53fa150e75486084861f6d2826a35bb35610
devilkin <devilkin@13be5e6b-9a2a-0410-ba90-e03f93e6454f>
2007-04-24 20:07:35 +0000

    added no_output publisher
    git-svn-id: https://phptelemeter.svn.sourceforge.net/svnroot/phptelemeter/trunk@181 13be5e6b-9a2a-0410-ba90-e03f93e6454f

---
+++ modules/publisher_no_output.inc.php
@@ -0,0 +1,51 @@
+<?php
+
+if (! defined("_phptelemeter")) exit();
+
+define("_phptelemeter_publisher", "no_output");
+define("_phptelemeter_publisher_version", "1");
+/*
+
+phpTelemeter - a php script to read out and display ISP's usage-meter stats.
+
+publisher_plaintext_graphonly.inc.php - file which contains the plaintext publisher, graph only version
+
+Copyright (C) 2005 - 2007 Jan De Luyck  <jan -at- kcore -dot- org>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+The full text of the license can be found here:
+http://www.gnu.org/licenses/gpl.txt
+
+*/
+
+require_once("libs/phptelemeter_publisher_shared.inc.php");
+
+class telemeterPublisher extends telemeterPublisher_shared
+{
+	function telemeterPublisher()
+	{
+		/* call parent constructor */
+		telemeterPublisher_shared::telemeterPublisher_shared();
+	}
+
+	function publishData($data, $showRemaining, $showDaily, $showGraph, $showResetDate)
+	{
+		return ("");
+	}
+
+	function newVersion($versionNr)
+	{
+		return("");
+	}
+}
+
+?>
