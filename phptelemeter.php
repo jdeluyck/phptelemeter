eb9c1d32e074a269b5e91e8a89e055c6a0dc2c27
devilkin <devilkin@13be5e6b-9a2a-0410-ba90-e03f93e6454f>
2007-07-14 10:03:37 +0000

    changed license to GPLv2 only (no more ..or later version)
    git-svn-id: https://phptelemeter.svn.sourceforge.net/svnroot/phptelemeter/trunk@218 13be5e6b-9a2a-0410-ba90-e03f93e6454f

--- phptelemeter.php
+++ phptelemeter.php
@@ -9,8 +9,7 @@ Copyright (C) 2005 - 2007 Jan De Luyck <jan -at- kcore -dot- org>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+the Free Software Foundation; version 2 of the License.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
